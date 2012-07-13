9120895518ce784e6347b7845245f42015f1e944
Bernardo Heynemann <heynemann@gmail.com>
2012-07-12 22:36:17 -0300

    Connection to redis and settings

---
+++ r3/web/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+DEBUG = True
+SECRET_KEY = 'development key'
+
+WEB_HOST = '0.0.0.0'
+WEB_PORT = 8888
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 7778
---
+++ r3/web/extensions.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import redis
+
+from flask import _app_ctx_stack as stack
+
+class RedisDB(object):
+
+    def __init__(self, app=None):
+        if app is not None:
+            self.app = app
+            self.init_app(self.app)
+        else:
+            self.app = None
+
+    def init_app(self, app):
+        app.config.setdefault('REDIS_HOST', '0.0.0.0')
+        app.config.setdefault('REDIS_PORT', 6379)
+        app.config.setdefault('REDIS_DB', 0)
+        app.config.setdefault('REDIS_PASS', None)
+
+        # Use the newstyle teardown_appcontext if it's available,
+        # otherwise fall back to the request context
+        if hasattr(app, 'teardown_appcontext'):
+            app.teardown_appcontext(self.teardown)
+        else:
+            app.teardown_request(self.teardown)
+
+    def connect(self):
+        options = {
+            'host': self.app.config['REDIS_HOST'],
+            'port': self.app.config['REDIS_PORT'],
+            'db': self.app.config['REDIS_DB']
+        }
+
+        if self.app.config['REDIS_PASS']:
+            options['password'] = self.app.config['REDIS_PASS']
+
+        conn = redis.StrictRedis(**options)
+        return conn
+
+    def teardown(self, exception):
+        ctx = stack.top
+        if hasattr(ctx, 'redis_db'):
+            ctx.redis_db.close()
+
+    @property
+    def connection(self):
+        ctx = stack.top
+        if ctx is not None:
+            if not hasattr(ctx, 'redis_db'):
+                ctx.redis_db = self.connect()
+            return ctx.redis_db
